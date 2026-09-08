baa14652776682d6681b8ef9280843a5a1bdee60
korismaska <korismaska@gmail.com>
2026-09-08 21:13:05 +0300

    Deploy Git updates into public_html so the main domain gets the new site.

--- scripts/cpanel-deploy.sh
+++ scripts/cpanel-deploy.sh
@@ -2,8 +2,8 @@
 # Runs from the cPanel Git clone directory.
 set -eu
 
-DEPLOYPATH="/home/korismas/maskastests.cpanel.site"
-APP_VENV="/home/korismas/nodevenv/maskastests.cpanel.site"
+DEPLOYPATH="/home/korismas/public_html"
+APP_VENV="/home/korismas/nodevenv/public_html"
 mkdir -p "$DEPLOYPATH/tmp"
 LOG="$DEPLOYPATH/tmp/deploy.log"
 exec >>"$LOG" 2>&1
@@ -131,17 +131,19 @@ fi
 pick_node() {
   NPM=""
   NODE=""
-  for version in 22 20 18 16; do
-    if [ -x "$APP_VENV/$version/bin/npm" ]; then
-      NPM="$APP_VENV/$version/bin/npm"
-      NODE="$APP_VENV/$version/bin/node"
-      return 0
-    fi
-    if [ -x "$APP_VENV/$version/bin/node" ]; then
-      NODE="$APP_VENV/$version/bin/node"
-      [ -x "$APP_VENV/$version/bin/npm" ] && NPM="$APP_VENV/$version/bin/npm"
-      return 0
-    fi
+  for venv in "$APP_VENV" /home/korismas/nodevenv/maskastests.cpanel.site; do
+    for version in 22 20 18 16; do
+      if [ -x "$venv/$version/bin/npm" ]; then
+        NPM="$venv/$version/bin/npm"
+        NODE="$venv/$version/bin/node"
+        return 0
+      fi
+      if [ -x "$venv/$version/bin/node" ]; then
+        NODE="$venv/$version/bin/node"
+        [ -x "$venv/$version/bin/npm" ] && NPM="$venv/$version/bin/npm"
+        return 0
+      fi
+    done
   done
   for candidate in \
     /opt/alt/alt-nodejs22/root/usr/bin/npm \
